597f6b04477ae3fe2bbcb14ba8b260f41c375b57
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-08-01 20:24:31 +0530

    Update connectMetamask2.js

--- connectMetamask2.js
+++ connectMetamask2.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 
-const connectMetamask2 = () => {
+const ConnectMetamask2 = () => {
+    const [walletAddress, setWalletAddress] = useState(null);
+
   const connect = async () => {
     if (typeof window.ethereum !== "undefined") {
       console.log("Metamask is installed");
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
-      console.log(accounts);
+      // console.log(accounts);
+      setWalletAddress(accounts[0]);
     }
   };
   return (
     <div className="App">
       <h1>connect Metamask2</h1>
+      <h2>Address: {walletAddress}</h2>
       <button onClick={connect}>connection 2</button>
     </div>
   );
 };
 
-export default connectMetamask2;
+export default ConnectMetamask2;
